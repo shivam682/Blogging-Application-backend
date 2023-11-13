0c999015a2ad75997ad933d8ea4f7d8347c79ec7
SHIVAM KUMAR JHA <86572907+shivam682@users.noreply.github.com>
2023-11-13 11:37:19 +0530

    Delete application directory

--- application/src/main/java/com/shivam/blog/application/repositries/CategoryRepo.java
+++
@@ -1,7 +0,0 @@
-package com.shivam.blog.application.repositries;
-
-import com.shivam.blog.application.models.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepo extends JpaRepository<Category,Integer> {
-}
